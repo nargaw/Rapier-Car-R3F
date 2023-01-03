269c5cf00315157a5e016562f9457ab480684ac4
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-01-03 14:37:00 -0500

    playing with physics

--- src/Experience.js
+++ src/Experience.js
@@ -7,6 +7,7 @@ export default function Experience()
     return <>
         <OrbitControls makeDefault />
         <Physics>
+            <Debug />
             <Test />
         </Physics>
     </>
--- src/Test.js
+++ src/Test.js
@@ -1,26 +1,54 @@
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, useImpulseJoint } from "@react-three/rapier";
+import { useRef } from "react";
+
+// const body1 = useRef()
+// const body2 = useRef()
+
+// const joint = useImpulseJoint(body1, body2)
+
+
 
 export default function Test()
 {
     return <>
         <RigidBody
             type="fixed"
+            // density={0.1}
+            
+            
         >   
             <mesh
                 rotation-x = {-Math.PI * 0.5}
             >
-                <boxGeometry args={[10, 10, 0.5]}/>
+                <boxGeometry args={[15, 15, 0.5]}/>
                 <meshBasicMaterial color={0xff0000} />
             </mesh>
         </RigidBody>
         
 
         <RigidBody
-            position={[0, 5, 0]}
+            // ref={body1}
+            position={[-2, 6, 0]}
+            // gravityScale={0.25}
+            friction={0.4}
+            restitution={0.8}
         >
             <mesh>
                 <boxGeometry args={[1, 1, 1]}/>
                 <meshBasicMaterial color={0xffff00} />
+            </mesh>
+        </RigidBody>
+
+        <RigidBody
+            // ref={body2}
+            position={[2, 6, 0]}
+            // gravityScale={0.25}
+            friction={0.4}
+            restitution={0.8}
+        >
+            <mesh>
+                <boxGeometry args={[1, 1, 1]}/>
+                <meshBasicMaterial color={0x00ffff} />
             </mesh>
         </RigidBody>
         
